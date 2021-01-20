193e5abf738ccef380af96da7f5f27bd57ab24fb
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-01-21 00:40:36 +0900

    create routers and controllers

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";  // default로 export한 것이 아님
+import userRouter from "./routers/userRouter";  // default로 export한 것이 아님
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
-
-// 마지막 함수는 next 인자가 없음, next : 다음 함수 실행
-const handleHome = (req, res) => res.send("✅ Hello Home");
-const handleProfile = (req, res) => res.send("✅ get profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -16,11 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);       // user에 userRouter 부여
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);       // users에 userRouter 부여
+app.use(routes.videos, videoRouter);
 
 export default app;
-
