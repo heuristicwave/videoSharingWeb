6d1188388f9d0b61d7e0fa8e6633f523f65acf21
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-01-24 16:23:03 +0900

    add Search Func (url, query, controller)

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,12 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+  //- const searchingBy = req.query.term; // Before ES6
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy }); //- searchingBy: searchingBy
+};
 
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
