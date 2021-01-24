f1f49e322065e17fc53c2cf8c6d4662e36bd8930
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-01-25 00:03:34 +0900

    add Users Control

--- middlewares.js
+++ middlewares.js
@@ -4,5 +4,10 @@ export const localsMiddleware = (req, res, next) => {
   // http://expressjs.com/ko/api.html#res.locals
   res.locals.siteName = "VSW";
   res.locals.routes = routes;
+  // sample for userDetail test
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1
+  };
   next();
 };
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,18 @@
 import express from "express";
 import routes from "../routes";
-import { users, userDetail, editProfile, changePassword } from "../controllers/userController";
+import {
+  users,
+  userDetail,
+  editProfile,
+  changePassword,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 //- 순서에 따라서 url 인식이 달라짐
-//- 예) userDetail - changePassword일경우, user/change-password를 userdetail로 인식 
+//- 예) userDetail - changePassword일경우, user/change-password를 userdetail로 인식
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
