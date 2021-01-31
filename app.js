e47f694cf81c5f47b035322488d07eab0c59be33
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-02-01 00:06:08 +0900

    edit typo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
-import userRouter from "./routers/userRouter";  // default로 export한 것이 아님
+import userRouter from "./routers/userRouter"; // default로 export한 것이 아님
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 const app = express();
@@ -17,10 +17,10 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(localsMiddleware);  // local var => globa var
+app.use(localsMiddleware); // local var => global var
 
 app.use(routes.home, globalRouter);
-app.use(routes.users, userRouter);       // /users에 userRouter 부여
-app.use(routes.videos, videoRouter);     // /videos에 videoRouter 부여
+app.use(routes.users, userRouter); // /users에 userRouter 부여
+app.use(routes.videos, videoRouter); // /videos에 videoRouter 부여
 
 export default app;
