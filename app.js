e7e5b2c343de2090223770652c8394387b207396
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-02-07 17:39:55 +0900

    Add Session and CookieStore

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter"; // default로 export한 것이 아님
@@ -14,6 +17,8 @@ import "./passport";
 
 const app = express();
 
+const CookieStore = MongoStore(session); // 세션 mongo에 저장
+
 app.use(helmet({ contentSecurityPolicy: false }));
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads")); // static() : directory에서 file을 전달하는 middleware
@@ -22,6 +27,16 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  // https://randomkeygen.com Seed 사용
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
