5f9761a01d2e4503e6b584d7e6c8938ec842ff20
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-02-08 00:02:16 +0900

    Add restrict access

--- middlewares.js
+++ middlewares.js
@@ -10,5 +10,23 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.user = req.user || null;
   next();
 };
+
+// login 후, 접근제어 관리
+export const onlyPublic = (req, res, next) => {
+  if (req.user) {
+    res.redirect(routes.home);
+  } else {
+    next();
+  }
+};
+
+export const onlyPrivate = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect(routes.home);
+  }
+};
+
 // upload.pug의 videoFile, 하나의 비디오 파일만 올라감
 export const uploadVideo = multerVideo.single("videoFile");
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,13 +6,14 @@ import {
   editProfile,
   changePassword,
 } from "../controllers/userController";
+import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
 //- 순서에 따라서 url 인식이 달라짐
 //- 예) userDetail - changePassword일경우, user/change-password를 userdetail로 인식
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
