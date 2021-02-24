cff0013c2c274fde5baa82c7a6044f62807df9d8
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-02-24 10:46:22 +0900

    Add record video stream

---
+++ assets/js/videoRecorder.js
@@ -0,0 +1,70 @@
+const recorderContainer = document.getElementById("jsRecordContainer");
+const recordBtn = document.getElementById("jsRecordBtn");
+const videoPreview = document.getElementById("jsVideoPreview");
+
+let streamObject;
+let videoRecorder;
+
+// download reocord data
+const handleVideoData = (event) => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  stopStreamedVideo(videoPreview);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+};
+
+function stopStreamedVideo(videoElem) {
+  const stream = videoElem.srcObject;
+  const tracks = stream.getTracks();
+
+  tracks.forEach(function (track) {
+    track.stop();
+  });
+
+  videoElem.srcObject = null;
+}
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
+};
+
+const getVideo = async () => {
+  try {
+    // https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: { width: 1280, height: 720 },
+    });
+    videoPreview.srcObject = stream;
+    videoPreview.muted = true;
+    videoPreview.play();
+    recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording();
+  } catch (error) {
+    recordBtn.innerHTML = " 😡 Can't record";
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
+  }
+};
+
+function init() {
+  recordBtn.addEventListener("click", getVideo);
+}
+
+if (recorderContainer) {
+  init();
+}
